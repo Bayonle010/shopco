77995ef1f01ec7ffc0dbf90c3d05c74f6490136d
Onadiran Bayonle <onadiranbayonle358@gmail.com>
2025-06-14 01:49:55 +0100

    configured generic response throughout application

--- src/main/java/com/shopco/common/response/ApiResponse.java
+++ src/main/java/com/shopco/common/response/ApiResponse.java
@@ -1,4 +1,26 @@
 package com.shopco.common.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ApiResponse {
+    private boolean status;
+    private int statusCode;
+    private String message;
+    private Object data;
+    private Object metadata;
+    private LocalDateTime timeStamp;
+
+    public ApiResponse(boolean status, int statusCode, String message, LocalDateTime timeStamp, Object metadata,  Object data) {
+        this.status = status;
+        this.statusCode = statusCode;
+        this.message = message;
+        this.timeStamp = timeStamp;
+        this.data = data;
+        this.metadata = metadata;
+    }
+
 }
