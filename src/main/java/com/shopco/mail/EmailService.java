7f99ec509f7565234cbc2c7c7f0836d1f275bcd6
Bayonle010 <afeezonadiran@gmail.com>
2025-11-28 01:02:08 +0100

    change email config to match

--- src/main/java/com/shopco/mail/EmailService.java
+++ src/main/java/com/shopco/mail/EmailService.java
@@ -57,7 +57,7 @@ public class EmailService {
         Context context = new Context();
         context.setVariables(properties);
 
-        helper.setFrom("noreply@shopco.com");
+        helper.setFrom("noreply@bayfiapp.com");
         helper.setTo(to);
         helper.setSubject(subject);
 
